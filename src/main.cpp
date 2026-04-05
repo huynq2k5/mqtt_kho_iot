1fbaa5db7003629aafd2ffc034531bf63cb43c14
huynq2k5 <huyn84341@gmail.com>
2026-04-05 16:05:08 +0700

    them cua so

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <PubSubClient.h>
 #include <DHT.h>
 #include <ArduinoJson.h>
+#include <LiquidCrystal_I2C.h>
 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
@@ -24,11 +25,13 @@ bool systemManual = false; // Cờ chế độ toàn cục
 #define RELAY_FAN 18
 #define RELAY_AC  17
 #define RELAY_HUM 16
+#define RELAY_CS 4
 #define DHTPIN 12
 #define DHTTYPE DHT22
 #define LDR_PIN 34
 #define CO2_PIN 36
 
+LiquidCrystal_I2C lcd(0x27, 20, 4); 
 DHT dht(DHTPIN, DHTTYPE);
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
@@ -42,7 +45,8 @@ KichBan dsKichBan[5];
 int soLuongKichBan = 0;
 float lastT = 0, lastH = 0;
 float lastCO2 = 0, lastLux = 0;
-bool statusFan = false, statusAC = false, statusHum = false;
+bool statusFan = false, statusAC = false, statusHum = false; 
+bool statusWindow = false;
 bool manualMode[3] = {false, false, false}; 
 
 SemaphoreHandle_t xMutex;
@@ -58,6 +62,17 @@ float docGiaTriLux() {
   return giaTriLux;
 }
 
+void hienThiLCD(float t, float h, float co2, float lux) {
+  lcd.setCursor(0, 0);
+  lcd.print("Nhiet do: "); lcd.print(t, 1); lcd.print(" C");
+  lcd.setCursor(0, 1);
+  lcd.print("Do am   : "); lcd.print(h, 1); lcd.print(" %");
+  lcd.setCursor(0, 2);
+  lcd.print("Anh sang: "); lcd.print(lux, 0); lcd.print(" Lux");
+  lcd.setCursor(0, 3);
+  lcd.print("Khi CO2 : "); lcd.print(co2, 0); lcd.print(" ppm");
+}
+
 void publishState(float t, float h, float co2, float lux) {
   StaticJsonDocument<256> doc;
   doc["t"] = t;
@@ -67,6 +82,7 @@ void publishState(float t, float h, float co2, float lux) {
   doc["fan"] = statusFan ? 1 : 0;
   doc["ac"] = statusAC ? 1 : 0;
   doc["hum"] = statusHum ? 1 : 0;
+  doc["win"] = statusWindow ? 1 : 0;
   doc["s"] = 1;
 
   char buffer[256];
@@ -105,6 +121,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
         String out = v["out"].as<String>();
         if (out == "q") dsKichBan[i].chanRa = RELAY_FAN;
         else if (out == "a") dsKichBan[i].chanRa = RELAY_AC;
+        else if (out == "cs") dsKichBan[i].chanRa = RELAY_CS;
         else dsKichBan[i].chanRa = RELAY_HUM;
         dsKichBan[i].hanhDong = (v["act"] == "ON") ? 1 : 0;
       }
@@ -121,8 +138,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
         if (device == "q") { statusFan = act; digitalWrite(RELAY_FAN, act); }
         else if (device == "a") { statusAC = act; digitalWrite(RELAY_AC, act); }
         else if (device == "h") { statusHum = act; digitalWrite(RELAY_HUM, act); }
+        else if (device == "cs") { statusWindow = act; digitalWrite(RELAY_CS, act); }
         
-        // SỬA LỖI 1: Thêm đủ 4 tham số (đọc giá trị hiện tại của CO2/LDR)analogRead
         publishState(dht.readTemperature(), dht.readHumidity(), analogRead(CO2_PIN), docGiaTriLux());
     }
   }
@@ -167,6 +184,7 @@ void TaskLogic(void *pvParameters) {
     float h = dht.readHumidity();
     float co2 = (float)analogRead(CO2_PIN);
     float lux = docGiaTriLux();
+    hienThiLCD(t, h, co2, lux);
 
     if (!isnan(t) && !isnan(h)) {
       bool coThayDoi = false;
@@ -184,7 +202,7 @@ void TaskLogic(void *pvParameters) {
 
       if (xSemaphoreTake(xMutex, portMAX_DELAY)) {
         if(!systemManual){
-            bool nextFan = false; bool nextAC = false; bool nextHum = false;
+            bool nextFan = false; bool nextAC = false; bool nextHum = false; bool nextCS = false;
             for (int i = 0; i < soLuongKichBan; i++) {
               bool trigger = false;
               float val = 0;
@@ -200,14 +218,16 @@ void TaskLogic(void *pvParameters) {
                 if (dsKichBan[i].chanRa == RELAY_FAN) nextFan = (dsKichBan[i].hanhDong == 1);
                 if (dsKichBan[i].chanRa == RELAY_AC) nextAC = (dsKichBan[i].hanhDong == 1);
                 if (dsKichBan[i].chanRa == RELAY_HUM) nextHum = (dsKichBan[i].hanhDong == 1);
+                if (dsKichBan[i].chanRa == RELAY_CS) nextCS = (dsKichBan[i].hanhDong == 1);
               }
             }
 
-            if (nextFan != statusFan || nextAC != statusAC || nextHum != statusHum || coThayDoi) {
-              statusFan = nextFan; statusAC = nextAC; statusHum = nextHum;
+            if (nextFan != statusFan || nextAC != statusAC || nextHum != statusHum || nextCS != statusWindow || coThayDoi) {
+              statusFan = nextFan; statusAC = nextAC; statusHum = nextHum; statusWindow = nextCS;
               digitalWrite(RELAY_FAN, statusFan);
               digitalWrite(RELAY_AC, statusAC);
               digitalWrite(RELAY_HUM, statusHum);
+              digitalWrite(RELAY_CS, statusWindow);
               publishState(t, h, co2, lux);
             }
         }
@@ -220,6 +240,9 @@ void TaskLogic(void *pvParameters) {
 
 void setup() {
   Serial.begin(115200);
+  lcd.init(); 
+  lcd.backlight();
+  pinMode(RELAY_CS, OUTPUT);
   pinMode(RELAY_FAN, OUTPUT); pinMode(RELAY_AC, OUTPUT); pinMode(RELAY_HUM, OUTPUT);
   dht.begin();
   xMutex = xSemaphoreCreateMutex();
